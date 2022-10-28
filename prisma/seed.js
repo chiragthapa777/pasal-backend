c25f406e57d297e2161ff21d621fcd9ae10a50e9
chiragthapa777 <chiragthapa777@gmail.com>
2022-10-28 12:11:34 +0545

    tags seeding and start script

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,25 +1,50 @@
-const { PrismaClient } = require('@prisma/client')
-const prisma = new PrismaClient()
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
 async function main() {
-  const alice = await prisma.otherCharge.upsert({
-    where:{
-        name:'delivery'
-    },
-    update:{},
-    create: {
-      name:'delivery',
-      desc:"charge for delivery",
-    price:100
-    },
-  })
+	const alice = await prisma.otherCharge.upsert({
+		where: {
+			name: "delivery",
+		},
+		update: {},
+		create: {
+			name: "delivery",
+			desc: "charge for delivery",
+			price: 100,
+		},
+	});
+	const tagsArray = [
+		"Women's Fashion",
+		"Health & Beauty",
+		"Men's Fashion",
+		"Watches, Bags, Jewellery",
+		"Electronic Devices",
+		"TV & Home Appliances",
+		"Electronic Accessories",
+		"Groceries & Pets",
+		"Babies & Toys",
+		"Home & Lifestyle",
+		"Sports & Outdoor",
+		"Motors, Tools & DIY",
+	];
+  for (const tag of tagsArray){
+    const tags = await prisma.tag.upsert({
+      where: {
+        name: tag
+      },
+      update: {},
+      create: {
+        name: tag
+      },
+    });
+  }
 }
 main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+	.then(async () => {
+		await prisma.$disconnect();
+	})
+	.catch(async (e) => {
+		console.error(e);
+		await prisma.$disconnect();
+		process.exit(1);
+	});
